ec04f319c8853d3791078a2809c734d278be5d22
BuildTools <unconfigured@null.spigotmc.org>
2020-04-27 22:26:20 +0200

    first commit, login in login style

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  
+  
+  #index page / root url
+  root 'pages#home'
+
+  devise_for :users
+  
+  
+  get 'pages/home'
+  get 'pages/login'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
